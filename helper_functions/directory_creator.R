2c62b7e0633629987ba98e07395a3f1b58ecd5de
Louis <s_cousilou@gvsu.edu>
2026-05-24 17:34:55 -0400

    Added code to allow for NA values in provided path. NA values will default to the previous location.

--- helper_functions/directory_creator.R
+++ helper_functions/directory_creator.R
@@ -1,39 +1,40 @@
 # Author: Louis Cousino
 # Date: 07-27-2025
-# Description: A function for creating nested folders. The function takes the base folder bath and creates sub folders
+# Description: A function for creating nested folders. The function takes the base folder path and creates sub folders
 # named after the subsequent arguments.
-
-
-
-
 
 directory_creator <- function(base_directory, ...) {
   
   path <- list(...)
   
   # Removing entries that have at least one dot (indicating a file)
-  
   path <- path[!stringr::str_detect(path, "\\.")]
   
+  # Converting NA values into blank
+  path[is.na(path)] <- ""
+  
+  # Initializing current path as provided base path
   current_path <- base_directory
   
   for (i in 1:length(path)) {
     
+    # Adding folder to path chain
     current_path <- stringr::str_c(current_path, "/", path[[i]])
     
     # Removing excess slashes
-    
     current_path <- stringr::str_replace_all(current_path, "/+", "/")
     
+    # Removing hanging slashes at the end (Happens when NA is the last value provided)
+    current_path <- stringr::str_replace_all(current_path, "/$", "")
+    
     if (dir.exists(current_path) == FALSE) {
-      
+
       dir.create(current_path)
-      
+
     }
     
   }
   
   return(current_path)
-  
   
 }
